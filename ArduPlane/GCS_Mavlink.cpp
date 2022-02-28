fedfa0eab1090f29c54e24942640530641ca1721
Tolga <tolgakarakurt@gmail.com>
2022-02-28 13:09:35 -0800

    cleanup: removed unwanted comments

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -1334,7 +1334,7 @@ void GCS_MAVLINK_Plane::handleMessage(const mavlink_message_t &msg)
         break;
     }
 
-//Customization//
+
     case MAVLINK_MSG_ID_SET_POSITION_TARGET_LOCAL_NED:
     {
         // decode packet
@@ -1345,11 +1345,6 @@ void GCS_MAVLINK_Plane::handleMessage(const mavlink_message_t &msg)
         if (plane.control_mode != &plane.mode_guided) {
             break;
         }
-
-        // exit if vehicle is not in vtol mode
-        // if (!plane.quadplane.in_vtol_mode()) {
-        //     break;
-        // }
 
         // check for supported coordinate frames
         if (packet.coordinate_frame != MAV_FRAME_LOCAL_NED &&
@@ -1377,7 +1372,6 @@ void GCS_MAVLINK_Plane::handleMessage(const mavlink_message_t &msg)
             // rotate to body-frame if necessary
             if (packet.coordinate_frame == MAV_FRAME_BODY_NED ||
                 packet.coordinate_frame == MAV_FRAME_BODY_OFFSET_NED) {
-                // assuming rotate_body_frame_to_NE is implemented
                 plane.rotate_body_frame_to_NE(pos_vector.x, pos_vector.y);
             }
             // add body offset if necessary
@@ -1419,76 +1413,12 @@ void GCS_MAVLINK_Plane::handleMessage(const mavlink_message_t &msg)
         if (pos_ignore && !vel_ignore) {
             plane.quadplane.set_velocity_setpoint(vel_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
         }
-        // Not yet implemmented
-        //  else if (!pos_ignore && vel_ignore) {
+        // else if (!pos_ignore && vel_ignore) {
         //     plane.mode_guided.set_destination(pos_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
         // }
-        // just do altitude for now
-        // plane.next_WP_loc.alt += -packet.z*100.0;
-        // gcs().send_text(MAV_SEVERITY_INFO, "Change alt to %.1f",
-        //                 (double)((plane.next_WP_loc.alt - plane.home.alt)*0.01));
-        
-
-        
-
-/*
-        // prepare position
-        Vector3f pos_vector;
-        if (!pos_ignore) {
-            // convert to cm
-            pos_vector = Vector3f(packet.x * 100.0f, packet.y * 100.0f, -packet.z * 100.0f);
-            // rotate to body-frame if necessary
-            if (packet.coordinate_frame == MAV_FRAME_BODY_NED) {
-                plane.rotate_body_frame_to_NE(pos_vector.x, pos_vector.y);
-            }
-            // add body offset if necessary
-            if (packet.coordinate_frame == MAV_FRAME_LOCAL_OFFSET_NED ||
-                packet.coordinate_frame == MAV_FRAME_BODY_NED ) {
-                pos_vector += plane.quadplane.inertial_nav.get_position();
-            }
-        }
-
-        // prepare velocity
-        Vector3f vel_vector;
-        if (!vel_ignore) {
-            // convert to cm
-            vel_vector = Vector3f(packet.vx * 100.0f, packet.vy * 100.0f, -packet.vz * 100.0f);
-            // rotate to body-frame if necessary
-            if (packet.coordinate_frame == MAV_FRAME_BODY_NED || 
-                packet.coordinate_frame == MAV_FRAME_BODY_OFFSET_NED) {
-                plane.rotate_body_frame_to_NE(vel_vector.x, vel_vector.y);
-            }
-        }
-*/
-/*
-        // prepare yaw
-        float yaw_cd = 0.0f;
-        bool yaw_relative = false;
-        float yaw_rate_cds = 0.0f;
-        if (!yaw_ignore) {
-            yaw_cd = ToDeg(packet.yaw) * 100.0f;
-            yaw_relative = packet.coordinate_frame == MAV_FRAME_BODY_NED || packet.coordinate_frame == MAV_FRAME_BODY_OFFSET_NED;
-        }
-        if (!yaw_rate_ignore) {
-            yaw_rate_cds = ToDeg(packet.yaw_rate) * 100.0f;
-        }
-*/
-        // send request
-        //if (!pos_ignore && !vel_ignore) {
-        //    plane.mode_guided.set_destination_posvel(pos_vector, vel_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
-        //} else if (pos_ignore && !vel_ignore) {
-        //    plane.mode_guided.set_velocity(vel_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
-        //} else if (!pos_ignore && vel_ignore) {
-        //    plane.mode_guided.set_destination(pos_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
-        //}
-
-        //if (pos_ignore && !vel_ignore) {
-        //    plane.mode_guided.set_velocity(vel_vector, !yaw_ignore, yaw_cd, !yaw_rate_ignore, yaw_rate_cds, yaw_relative);
-        //}        
 
         break;
     }
-//Customization//
 
     case MAVLINK_MSG_ID_SET_POSITION_TARGET_GLOBAL_INT:
     {
